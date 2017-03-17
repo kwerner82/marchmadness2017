8f1ca72782d0d63cff4c3e51540ac9b50ca6ae90
kwerner82 <wernenator@gmail.com>
2017-03-17 16:12:40 -0700

    Did half the names, still need to finish

--- Scripts/chuckpool.js
+++ Scripts/chuckpool.js
@@ -2,28 +2,28 @@ $(document).ready(function() {
     var winningTeamNumbers = [1, 5, 8, 6, 3, 4, 9, 2, 7, 0];
     var losingTeamNumbers = [4, 3, 5, 2, 7, 0, 8, 6, 9, 1];
 
-    var costPerSquare = 10;
+    var costPerSquare = 50;
 
     var payoutsPerRound = {};
     payoutsPerRound[0] = 0;
-    payoutsPerRound[1] = 5;
-    payoutsPerRound[2] = 10;
-    payoutsPerRound[3] = 20;
-    payoutsPerRound[4] = 40;
-    payoutsPerRound[5] = 80;
-    payoutsPerRound[6] = 200;
+    payoutsPerRound[1] = 25;
+    payoutsPerRound[2] = 50;
+    payoutsPerRound[3] = 100;
+    payoutsPerRound[4] = 200;
+    payoutsPerRound[5] = 400;
+    payoutsPerRound[6] = 1000;
 
     var players = [
-        ["KW  ", "RG  ", "DWE ", "CR  ", "SW  ", "AK  ", "RJ  ", "MV  ", "NC  ", "JW  "],
-        ["MS  ", "KW  ", "OG  ", "MGU ", "DWE ", "CW  ", "AK  ", "SW  ", "MV  ", "NC  "],
-        ["EL  ", "OG  ", "KW  ", "RG  ", "CW  ", "CP  ", "MGU ", "AK  ", "SB  ", "GS  "],
-        ["OG  ", "EL  ", "CR  ", "KW  ", "RG  ", "JW  ", "MKG ", "JF  ", "AK  ", "RJ  "],
-        ["PK  ", "CR  ", "EL  ", "MS  ", "KW  ", "RG  ", "NW  ", "JF  ", "JF  ", "AK  "],
-        ["CR  ", "GS  ", "MO  ", "EL  ", "EG  ", "PK  ", "RG  ", "NW  ", "CP  ", "MKG "],
-        ["RJ  ", "MO  ", "MS  ", "MV  ", "JB  ", "NC  ", "PK  ", "MKG ", "MGU ", "CP  "],
-        ["MO  ", "RJ  ", "EG  ", "SW  ", "EL  ", "JB  ", "NC  ", "PK  ", "MS  ", "MGU "],
-        ["EG  ", "MV  ", "SW  ", "GB  ", "AG  ", "MS  ", "JB  ", "NC  ", "PK  ", "MO  "],
-        ["NW  ", "SW  ", "RJ  ", "AG  ", "MV  ", "GB  ", "CR  ", "MGU ", "MO  ", "SB  "]
+        ["CW    ", "Be   ", "AMD  ", "E+J  ", "KD  ", "    ", "    ", "    ", "    ", "    "],
+        ["Sch   ", "A+C  ", "KD   ", "Coach", "P   ", "    ", "    ", "    ", "    ", "    "],
+        ["BG    ", "MM   ", "Coach", "Sl   ", "RS  ", "    ", "    ", "    ", "    ", "    "],
+        ["Mr B  ", "Coach", "Fox  ", "Vil  ", "NJ  ", "    ", "    ", "    ", "    ", "    "],
+        ["Coach ", "K    ", "Sch  ", "MW   ", "Sl  ", "    ", "    ", "    ", "    ", "    "],
+        ["Dec M ", "NH   ", "Buck ", "Sch  ", "SD  ", "    ", "    ", "    ", "    ", "    "],
+        ["Bur   ", "McD  ", "SG   ", "A    ", "Sch ", "    ", "    ", "    ", "    ", "    "],
+        ["C     ", "Chief", "Vil  ", "E    ", "TM  ", "    ", "    ", "    ", "    ", "    "],
+        ["Sm    ", "JG   ", "Br   ", "BB   ", "G   ", "    ", "    ", "    ", "    ", "    "],
+        ["Hop   ", "TSr  ", "TSr  ", "G    ", "JDK ", "    ", "    ", "    ", "    ", "    "]
     ];
     
     initializeSquares(winningTeamNumbers, losingTeamNumbers, players, costPerSquare, payoutsPerRound);
