e164a6d8cdae1e61626d5a4dec4f825cfa0b771e
kwerner82 <wernenator@gmail.com>
2017-03-17 16:50:06 -0700

    Fixing an initial

--- Scripts/chuckpool.js
+++ Scripts/chuckpool.js
@@ -14,16 +14,16 @@ $(document).ready(function() {
     payoutsPerRound[6] = 1000;
 
     var players = [
-        ["CW    ", "Be   ", "AMD  ", "E+J  ", "KD  ", "D+C", "T       ", "K+L     ", "A+C ", "Hop "],
-        ["Sch   ", "A+C  ", "KD   ", "Coach", "P   ", "JK ", "K       ", "K       ", "DS  ", "C   "],
-        ["BG    ", "MM   ", "Coach", "Sl   ", "RS  ", "RH ", "Mr B    ", "PG+K    ", "TSr ", "TSr "],
-        ["Mr B  ", "Coach", "Fox  ", "Vil  ", "NJ  ", "Fa ", "GH+Coach", "M       ", "Br  ", "M+D "],
-        ["Coach ", "K    ", "Sch  ", "MW   ", "Sl  ", "R  ", "JD      ", "GH+Coach", "CJr ", "BH  "],
-        ["Dec M ", "NH   ", "Buck ", "Sch  ", "SD  ", "MV ", "D       ", "NH      ", "SG  ", "Cu  "],
-        ["Bur   ", "McD  ", "SG   ", "A    ", "Sch ", "JD ", "G       ", "DC      ", "C   ", "Cu  "],
-        ["C     ", "Chief", "Vil  ", "E    ", "TM  ", "G  ", "W       ", "Fa      ", "Fo  ", "JR  "],
-        ["Sm    ", "JG   ", "Br   ", "BB   ", "G   ", "K+L", "C       ", "BG      ", "D+C ", "Be  "],
-        ["Hop   ", "TSr  ", "TSr  ", "G    ", "JD+K", "BG ", "GM      ", "JG      ", "T   ", "DW  "]
+        ["CW    ", "Be   ", "AMD  ", "E+J  ", "KD  ", "D+C  ", "T       ", "K+L     ", "A+C ", "Hop "],
+        ["Sch   ", "A+C  ", "KD   ", "Coach", "P   ", "JK   ", "K       ", "K       ", "DS  ", "C   "],
+        ["BG    ", "MM   ", "Coach", "Sl   ", "RS  ", "RH   ", "Mr B    ", "PG+K    ", "TSr ", "TSr "],
+        ["Mr B  ", "Coach", "Fox  ", "Vil  ", "NJ  ", "Fa   ", "GH+Coach", "M       ", "Br  ", "M+D "],
+        ["Coach ", "K    ", "Sch  ", "MW   ", "Sl  ", "R    ", "JD      ", "GH+Coach", "CJr ", "BH  "],
+        ["Dec M ", "NH   ", "Buck ", "Sch  ", "SD  ", "MV   ", "D       ", "NH      ", "SG  ", "Cu  "],
+        ["Bur   ", "McD  ", "SG   ", "A    ", "Sch ", "JD   ", "G       ", "DC      ", "C   ", "Cu  "],
+        ["C     ", "Chief", "Vil  ", "E    ", "TM  ", "G    ", "W       ", "Fa      ", "Fo  ", "JR  "],
+        ["Sm    ", "JG   ", "Br   ", "BB   ", "G   ", "K+L  ", "C       ", "BG      ", "D+C ", "Be  "],
+        ["Hop   ", "TSr  ", "TSr  ", "G    ", "JD+K", "BrosG", "GM      ", "JG      ", "T   ", "DW  "]
     ];
     
     initializeSquares(winningTeamNumbers, losingTeamNumbers, players, costPerSquare, payoutsPerRound);
